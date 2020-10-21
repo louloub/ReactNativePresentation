e1d3d44a5830dcfba629888234eccc5ed328c5dc
loulou <yourdj.france@gmail.com>
2020-10-21 10:14:30 +0200

    second commit

--- App.js
+++ App.js
@@ -23,10 +23,27 @@ const App = () => {
       { cancelable: false }
     );
 
+    const buttonDefinition = () =>
+    Alert.alert(
+      "Quelques définitions",
+      "- On appelle framework front-end tout ensemble de classes, fonctions et utilitaires qui nous facilite la création d'applications. Un framework appelle notre propre code, là où c'est notre code qui appelle celui d'une bibliothèque.  \n - On appelle une librairie quand nous avons un besoin particulier. Nous pouvons ainsi réutiliser du code déjà existant pour une fonctionnalité précise \n - En informatique, le logiciel multiplateforme est un logiciel informatique implémenté sur plusieurs plates-formes informatiques. ",
+      [
+        {
+          text: "Cancel",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel"
+        },
+        { text: "OK", onPress: () => console.log("OK Pressed") }
+      ],
+      { cancelable: false }
+    );  
+
   return (
     <Center>      
       <Spacer />
       <Button title="Introduction" onPress={buttonIntroduction}  />
+      <Button title="Quelques définitions" onPress={buttonDefinition}  />
+  
       <Spacer />
     </Center>
   );
@@ -34,14 +51,8 @@ const App = () => {
 
 export default App
 
-/* 
-<CustomButton
-        text='Introduction'
-        type='outlined'
-        onPress={onPress}  
-      />      
-      
-  const onPress = () => {
-    alert('Introduction')
-  }
+/*
+<Button title="React & React Native" onPress={buttonReactAndReactNative}  />
+      <Button title="Qui utilise React Native" onPress={buttonWhosUseReactNative}  />
+      <Button title="Job" onPress={buttonJob}  />
 */
